57ba045263368fc160b9df877fde3c61f1192608
Lillyan <103754183+Liillyan@users.noreply.github.com>
2024-09-19 11:56:38 -0300

    Create ItemGeladeira.cs
    Adicionando classe ItemGeladeira

---
+++ ItemGeladeira.cs
@@ -0,0 +1,13 @@
+namespace ProjetoGeladeira
+{
+    public class ItemGeladeira
+    {
+        public string Nome { get; set; }
+
+        public ItemGeladeira(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
+
