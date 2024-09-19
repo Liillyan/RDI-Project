b0df3c198bc0e664ee0a82b498f94565e1796273
Lillyan <103754183+Liillyan@users.noreply.github.com>
2024-09-19 11:17:17 -0300

    Create Program.cs
    Atualizando o Program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoGeladeira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Geladeira geladeira = new Geladeira();
+            geladeira.Inicializar();
+            geladeira.ImprimirItens();
+
+            geladeira.RemoverElemento(0, 1, 1); 
+            geladeira.ImprimirItens();
+
+            geladeira.AdicionarElemento(0, 1, 0, "Alface"); 
+            geladeira.ImprimirItens();
+        }
+    }
+}
